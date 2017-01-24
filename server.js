bff449955de74fe1d493cb97e6fb9bf67aa9eeb8
Allan Guggenheim <allan.guggenheim@gmail.com>
2017-01-24 15:50:12 -0800

    almost done

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
-var express = require("express");
-var method-override = require("method-override");
-var body-parser = require("body-parser");
 
+var express = require("express");
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
+var expressHandlebars = require("express-handlebars");
+
+var app = express();
+var PORT = process.env.PORT || 8082;
+
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use(methodOverride("_method"));
+
+app.engine("handlebars", expressHandlebars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+// app.use("/", routes)
+
+app.listen(PORT, console.log("go to http://localhost:"+PORT));
